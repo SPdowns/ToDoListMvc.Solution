4dac310d3cccf97c609ce09bfbee6afd99ba3379
Sean Downs <spdowns@protonmail.com>
2020-07-23 09:58:43 -0700

    meaningful fail on test category. FAIL
    Co-authored-by: MarielHamson <mariel.hamson@gmail.com>

---
+++ ToDoListMvc.Tests/ModelTests/CategoriesTests.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoListMvc.Models;
+using System.Collections.Generic;
+using System;
+
+namespace ToDoListMvc.Tests
+{
+  [TestClass]
+  public class CategoryTest
+  {
+    [TestMethod]
+    public void CategoryConstructor_CreatesInstanceOfCategory_Category()
+    {
+      Category newCategory = new Category("test category");
+      Assert.AreEqual(typeof(Category), newCategory.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+      string result = newCategory.Name;
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsCategoryId_Int()
+    {
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+      int result = newCategory.Id;
+      Assert.AreEqual(1, result);
+    }
+  }
+}
