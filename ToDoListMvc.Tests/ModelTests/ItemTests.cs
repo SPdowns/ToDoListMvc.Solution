2c30e938b7faab0817eb1f4a13d75983fceff731
Sean Downs <spdowns@protonmail.com>
2020-07-23 09:43:53 -0700

    add Categories.cs and CategoriesTests.cs
    Co-authored-by: MarielHamson <mariel.hamson@gmail.com>

--- ToDoListMvc.Tests/ModelTests/ItemTests.cs
+++ ToDoListMvc.Tests/ModelTests/ItemTests.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using ToDoList.Models;
+using ToDoListMvc.Models;
 using System;
 
 namespace ToDoListMvc.Tests
